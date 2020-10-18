5df27cad4c6646b84c5f7c3da7d90b4f4daee4f8
Hansq3 <hansq3@gmail.com>
2020-10-18 09:43:01 +0200

    Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
+// Class Component -always use-
+
+/* class MyComponent extends React.Component {
+
+} */
+
+// Function Component 
+// React custom components always start with capital letter 
+// If you start with small letter, React will try to interpret as a HTML tag
+function IAmAH1() {
+  return <h1>Wow this is so cool!</h1> // if you writing in multiline always use ()
+}
+
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <IAmAH1 />
   );
 }
 
