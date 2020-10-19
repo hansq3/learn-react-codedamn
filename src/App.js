2bef7f858e51eeecc587abec0b05cb0341ee2cc6
Hansq3 <hansq3@gmail.com>
2020-10-19 06:44:18 +0200

    Events In React

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
 function App() {
+
+  function buttonWasClicked(event) {
+    console.log(event.target)
+    // alert('I was clicked!')
+  }
+
   return (
-    <h1>Hello World</h1>
+    <div>
+      <button onClick={buttonWasClicked}>Button</button>
+      <h1>Hello World</h1>
+    </div>
   );
 }
 
