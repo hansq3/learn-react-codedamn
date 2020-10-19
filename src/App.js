fe8772aefdd5205fce94f3cf2e321428e9e843dc
Hansq3 <hansq3@gmail.com>
2020-10-19 06:28:21 +0200

    JSX Expressions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
+// JSX Expressions inside {}
+// Valid JSX: null, numbers, HTML, strings, arrays
+// if else statement not work - use ternary operator - or JS function map()
+
+const arr = [1,2,3,4,5]
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <h1>Hello World { arr.map(val => <div>{val ** 2}</div>) }</h1>
   );
 }
 
