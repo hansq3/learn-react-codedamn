de7ecfec2340479ca8529f53ba7ae03673a5a029
Hansq3 <hansq3@gmail.com>
2020-10-25 11:49:47 +0100

    useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(5)   // returns array [counter, setCounter]  
+                                              // counter - value of current state variable
+                                              // setCounter - function which does allows to update are current state
+
+  function buttonWasClicked() {
+    //setCounter(2) // whatever value you pass in (2) that value is assigned to you first parameter(counter)
+    setCounter(counter + 1)
+  }
+
   return (
-    <h1>Hello World</h1>
+    <div>
+      <button onClick={buttonWasClicked}>Button</button>
+      <h1>{counter}</h1>
+    </div>
   );
 }
 
